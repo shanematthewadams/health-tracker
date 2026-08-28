ad192dddf61e55e18d6baebb3e3097e9e3076647
shanematthewadams <shanematthewadams@gmail.com>
2026-08-27 20:02:40 -0500

    Wrap app in friendly error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Tracker from './Tracker.jsx';
 import OnboardingGate from './OnboardingGate.jsx';
+import ErrorBoundary from './ErrorBoundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <OnboardingGate>
-      <Tracker />
-    </OnboardingGate>
+    <ErrorBoundary>
+      <OnboardingGate>
+        <Tracker />
+      </OnboardingGate>
+    </ErrorBoundary>
   </React.StrictMode>
 );
