c43f06335d3083bfb8de26ff46106e73e8197d6c
shanematthewadams <shanematthewadams@gmail.com>
2026-08-28 12:22:07 -0500

    Route public privacy policy

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import Tracker from './Tracker.jsx';
 import OnboardingGate from './OnboardingGate.jsx';
 import ErrorBoundary from './ErrorBoundary.jsx';
+import PrivacyPolicy from './PrivacyPolicy.jsx';
+
+const isPrivacy = window.location.pathname.replace(/\/+$/, "") === "/privacy";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <OnboardingGate>
-        <Tracker />
-      </OnboardingGate>
+      {isPrivacy ? (
+        <PrivacyPolicy />
+      ) : (
+        <OnboardingGate>
+          <Tracker />
+        </OnboardingGate>
+      )}
     </ErrorBoundary>
   </React.StrictMode>
 );
