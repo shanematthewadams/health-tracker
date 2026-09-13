74c0a10b56ddc2f6ca291dad465a9cf9ecfdd13f
shanematthewadams <shanematthewadams@gmail.com>
2026-09-13 08:28:04 -0500

    Remove pre-applied migration filename

--- supabase/migrations/20260913123000_fix_custom_metric_owner_select.sql
+++
@@ -1,8 +0,0 @@
-drop policy if exists "permitted people can view custom metrics" on public.custom_metrics;
-
-create policy "permitted people can view custom metrics"
-  on public.custom_metrics for select
-  using (
-    (select private.owns_profile(profile_id))
-    or (select private.can_view_custom_metric(id))
-  );
