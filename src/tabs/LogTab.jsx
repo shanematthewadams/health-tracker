6367b4c6e1d8c7240d23e37f8170a2b4373367c9
shanematthewadams <shanematthewadams@gmail.com>
2026-09-14 17:42:49 -0500

    Add fasting start access to Log

--- src/tabs/LogTab.jsx
+++ src/tabs/LogTab.jsx
@@ -1,6 +1,8 @@
 import LogTabBase from "./LogTabBase.jsx";
 import CustomTrackersLogSection from "../components/CustomTrackersLogSection.jsx";
 import FastingHistorySection from "../components/FastingHistorySection.jsx";
+import FastingTodaySection from "../components/FastingTodaySection.jsx";
+import { useOwnTrackerPreferences } from "../useOwnTrackerPreferences.js";
 
 export default function LogTab(props) {
   const initialDate = {
@@ -10,6 +12,9 @@ export default function LogTab(props) {
     water: props.waterDate,
     steps: props.stepsDate,
   }[props.logTab] || props.today;
+  const { trackerEnabled } = useOwnTrackerPreferences(props.activeCanEdit);
+  const activeFast = props.activeFasts?.[props.activeUser] || null;
+  const fastingVisible = trackerEnabled("fasting") || Boolean(activeFast);
 
   return (
     <>
@@ -20,6 +25,27 @@ export default function LogTab(props) {
         initialDate={initialDate}
         styles={props.styles}
       />
+      {props.activeCanEdit && (
+        <FastingTodaySection
+          activeFast={activeFast}
+          visible={fastingVisible}
+          editorOpen={props.fastEditorOpen}
+          fastBusy={props.fastBusy}
+          startDate={props.fastStartDate}
+          startTime={props.fastStartTime}
+          setStartDate={props.setFastStartDate}
+          setStartTime={props.setFastStartTime}
+          setEditorOpen={props.setFastEditorOpen}
+          openEditor={props.openFastEditor}
+          startFast={props.startFast}
+          updateFastStart={props.updateFastStart}
+          endFast={props.endFast}
+          today={props.today}
+          timeZone={props.timeZone}
+          styles={props.styles}
+          showStartAction
+        />
+      )}
       <FastingHistorySection
         activeCanEdit={props.activeCanEdit}
         styles={props.styles}
